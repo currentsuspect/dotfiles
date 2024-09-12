b2b7d6cf8a6d1d22bde1488cce2e641d7bfbffc5
currentsuspect <makoridylan@gmail.com>
2024-09-12 14:00:00 +0300

    Daily sync

--- nvim/.config/nvim/lua/core/cmp.lua
+++ nvim/.config/nvim/lua/core/cmp.lua
@@ -2,6 +2,7 @@
 
 local cmp = require('cmp')
 local luasnip = require('luasnip')
+local lspkind = require('lspkind')
 
 cmp.setup({
     snippet = {
@@ -15,12 +16,26 @@ cmp.setup({
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        ['<Tab>'] = cmp.mapping.select_next_item(),
     }),
     sources = {
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
         { name = 'buffer' },
         { name = 'path' },
+    },
+    formatting = {
+        format = lspkind.cmp_format({
+            with_text = true,  -- Show text alongside icons
+            maxwidth = 50,     -- Prevent overflow
+            menu = ({
+                buffer = "[Buffer]",
+                nvim_lsp = "[LSP]",
+                luasnip = "[Snippet]",
+                path = "[Path]",
+            })
+        }),
     },
 })
 
